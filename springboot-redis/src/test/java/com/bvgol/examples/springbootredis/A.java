85901a09bdcd48624aaae89852f1bb68f1b85b2d
郭辰 <bvg_winnir@163.com>
2020-06-04 14:17:30 +0800

    挖坑必备

---
+++ springboot-redis/src/test/java/com/bvgol/examples/springbootredis/A.java
@@ -0,0 +1,17 @@
+package com.bvgol.examples.springbootredis;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Classname A
+ * @Description TODO
+ * @Date 2020/5/25 9:15
+ * @Created by guochen
+ */
+@Slf4j
+
+public class A {
+    public static void main(String[] args) {
+        // \u000d System.out.println("Hello World!");
+    }
+}
