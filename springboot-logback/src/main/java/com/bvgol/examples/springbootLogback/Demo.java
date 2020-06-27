ed3768efb0d51a11a25f8a8089b7cc7b271e579a
GUOCHEN <bvg_winnir@163.com>
2020-06-27 21:28:17 +0800

    logback init

---
+++ springboot-logback/src/main/java/com/bvgol/examples/springbootLogback/Demo.java
@@ -0,0 +1,23 @@
+package com.bvgol.examples.springbootLogback;
+
+import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author guochen
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/6/2721:22
+ */
+public class Demo {
+ //这里的包可能没有引对
+ private Logger xiaojia = (Logger) LoggerFactory.getLogger("xiaojia");
+
+
+}
+@Slf4j(topic = "xiaojia")
+ class XiaoJiaTest {
+
+}
---
+++ springboot-logback/src/main/java/com/bvgol/examples/springbootLogback/LogbackApplication.java
@@ -0,0 +1,18 @@
+package com.bvgol.examples.springbootLogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guochen
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/6/2721:26
+ */
+@SpringBootApplication
+public class LogbackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackApplication.class, args);
+    }
+}
