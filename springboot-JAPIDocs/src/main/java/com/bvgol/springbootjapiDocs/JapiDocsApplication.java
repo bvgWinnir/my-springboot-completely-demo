bf9a9d82227a479e230f90f6876a0e594397d184
GUOCHEN <bvg_winnir@163.com>
2020-06-27 21:52:26 +0800

    japidocs  init2

---
+++ springboot-JAPIDocs/src/main/java/com/bvgol/springbootjapiDocs/JapiDocsApplication.java
@@ -0,0 +1,19 @@
+package com.bvgol.springbootjapiDocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guochen
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/6/2721:46
+ */
+@SpringBootApplication
+public class JapiDocsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JapiDocsApplication.class, args);
+        // 官网   https://japidocs.agilestudio.cn/#/zh-cn/?id=apidoc
+    }
+}
---
+++ springboot-JAPIDocs/src/main/java/com/bvgol/springbootjapiDocs/demo/ProductDoc.java
@@ -0,0 +1,17 @@
+package com.bvgol.springbootjapiDocs.demo;
+
+import io.github.yedaxia.apidocs.Docs;
+import io.github.yedaxia.apidocs.DocsConfig;
+
+public class ProductDoc {
+    public static void main(String[] args) {
+        DocsConfig config = new DocsConfig();
+        config.setProjectPath("D:\\xxxx\\src\\main\\java\\com"); // 项目根目录
+        config.setProjectName("push_robot_server"); // 项目名称
+        config.setApiVersion("V1.0");       // 声明该API的版本
+        config.setDocsPath("D:\\doc"); // 生成API 文档所在目录
+        config.setAutoGenerate(Boolean.TRUE);  // 配置自动生成
+        config.setCodeTplPath("classpath:doc/api-controller.html.ftl");
+        Docs.buildHtmlDocs(config); // 执行生成文档
+    }
+}
