5b270a766372cf969740f35c7e0d0388e50ca522
guochen <bvg_Winnir@163.com>
2020-08-27 11:55:05 +0800

    热部署  devtools 方式 测试 修改类–>保存：应用会重启 修改配置文件–>保存：应用会重启  √ 修改页面–>保存：应用不会重启，但会重新加载，页面会刷

---
+++ springboot-devtools/src/main/java/com/bvgol/devtools/demo/SpringbootDevtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.bvgol.devtools.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDevtoolsApplication.class, args);
+    }
+
+}
