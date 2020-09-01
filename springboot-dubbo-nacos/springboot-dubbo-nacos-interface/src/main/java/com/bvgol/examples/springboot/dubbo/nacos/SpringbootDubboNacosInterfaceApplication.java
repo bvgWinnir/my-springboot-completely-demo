43b03167194ddd580df7d15bf5daba0e757a2c25
guochen <bvg_Winnir@163.com>
2020-09-01 15:23:35 +0800

    增加启动类

---
+++ springboot-dubbo-nacos/springboot-dubbo-nacos-interface/src/main/java/com/bvgol/examples/springboot/dubbo/nacos/SpringbootDubboNacosInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.bvgol.examples.springboot.dubbo.nacos;
+
+/**
+ * @author guochen
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/6/2811:18
+ */
+public class SpringbootDubboNacosInterfaceApplication {
+    public static void main(String[] args) {
+    }
+}
