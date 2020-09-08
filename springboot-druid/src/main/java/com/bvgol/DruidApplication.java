a0c39b66b420ff259ded54f47dbf121db7ae94e1
guochen <bvg_Winnir@163.com>
2020-09-08 17:19:41 +0800

    druid init

---
+++ springboot-druid/src/main/java/com/bvgol/DruidApplication.java
@@ -0,0 +1,19 @@
+
+package com.bvgol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname DruidApplication
+ * @Description TODO
+ * @Date 2020/9/8 15:40
+ * @Created by GUOCHEN
+ */
+@SpringBootApplication
+public class DruidApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DruidApplication.class, args);
+    }
+
+//    http://localhost:8080/druid/login.html
+}
