7b2f9faf947496536d7f321e2705112333c3541c
guochen <bvg_Winnir@163.com>
2020-10-29 11:23:53 +0800

    修改了定时任务

--- springboot-docker/src/main/java/com/bvgol/examples/springbootdocker/controller/DynamicScheduleTask.java
+++ springboot-docker/src/main/java/com/bvgol/examples/springbootdocker/controller/DynamicScheduleTask.java
@@ -19,6 +19,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -49,22 +51,45 @@ public class DynamicScheduleTask implements SchedulingConfigurer {
                 triggerContext -> {
                     //2.1 从数据库获取执行周期
 
+                    long start = System.currentTimeMillis();
 
                     Cron cron = cronService.getById(1L);
                     //2.2 合法性校验.
-                    if (StringUtils.isEmpty(cron.getCron())) {
+                    if (!StringUtils.isEmpty(cron.getCron())) {
 
-                        User user = new User();
-                        user.setUsername(UUID.randomUUID().toString().substring(0,4));
-                        user.setPassword(UUID.randomUUID().toString().substring(0,4));
-                        user.setSalt(UUID.randomUUID().toString().substring(0,4));
-                        user.setEmail(UUID.randomUUID().toString().substring(0,4));
-                        user.setMobile(UUID.randomUUID().toString().substring(0,4));
-                        user.setStatus((byte)0);
-                        user.setDeptId(0L);
-                        user.setCreateTime(new Date());
+                        List<User> list = new LinkedList<>();
 
-                        userService.save(user);
+                        for (int i = 0; i <10000 ; i++) {
+                            User user = new User();
+                            user.setUsername(UUID.randomUUID().toString().substring(0,4));
+                            user.setPassword(UUID.randomUUID().toString().substring(0,4));
+                            user.setSalt(UUID.randomUUID().toString().substring(0,4));
+                            user.setEmail(UUID.randomUUID().toString().substring(0,4));
+                            user.setMobile(UUID.randomUUID().toString().substring(0,4));
+                            user.setStatus((byte)0);
+                            user.setDeptId(0L);
+                            user.setCreateTime(new Date());
+                            list.add(user);
+                        }
+
+                        long end = System.currentTimeMillis();
+                        log.info("构造10000对象耗时"+(end-start)+"ms");
+//                        userService.save(user);
+                        long startsaveBatch = System.currentTimeMillis();
+                        userService.saveBatch(list);
+                        long endsaveBatch = System.currentTimeMillis();
+                        log.info("批量添加耗时"+(endsaveBatch-startsaveBatch)+"ms");
+
+                        int count = userService.count();
+                        log.info("当前数据行:"+count);
+                        log.info("总耗时"+(System.currentTimeMillis()-start)+"ms");
+
+                        /***
+                         * 这里会出现   定时任务的时间步长小于执行时间
+                         * 1.算出第一次的时间 然后反向更新 定时任务时间 同时需要发送邮件和短信等通知相关人员
+                         *   不行 因为有可能已经积累多次定时任务(如何验证)
+                         *   在预期的时间内没有完成 就先去更新定时任务 然后发送通知
+                         * */
                     }
                     //2.3 返回执行周期(Date)
                     return new CronTrigger(cron.getCron()).nextExecutionTime(triggerContext);
---
+++ springboot-docker/src/main/java/com/bvgol/examples/springbootdocker/service/ActResult.java
@@ -0,0 +1,9 @@
+package com.bvgol.examples.springbootdocker.service;
+
+import lombok.Data;
+
+@Data
+public class ActResult {
+    private String code;
+
+}
