131d987ccd257e13f2d59c6cf71dac4b57af8af1
guochen <bvg_Winnir@163.com>
2020-11-16 15:56:53 +0800

    maven 结构调整 熟悉以下 maven 的pom依赖 jpa

---
+++ springboot-data/db/springboot-jpa/src/main/java/com/bvgol/examples/springbootjpa/controller/JpaController.java
@@ -0,0 +1,51 @@
+package com.bvgol.examples.springbootjpa.controller;
+
+import com.bvgol.examples.springbootjpa.dao.HBookDao;
+import com.bvgol.examples.springbootjpa.pojo.Hbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author 黄大娘
+ * @company dogsun公司
+ * @create 2019-11-13 20:24
+ */
+@RestController
+@RequestMapping("/jpa")
+public class JpaController {
+    @Autowired
+    private HBookDao hHBookDao;
+
+    @RequestMapping("/add")
+    public String add(Hbook hbook) {
+        hHBookDao.save(hbook);
+        return "success";
+    }
+
+    @RequestMapping("/edit")
+    public String edit(Hbook hbook) {
+        hHBookDao.save(hbook);
+        return "success";
+    }
+
+    @RequestMapping("/del")
+    public String del(Hbook hbook) {
+        hHBookDao.delete(hbook);
+        return "success";
+    }
+
+    @RequestMapping("/getOne")
+    public Hbook getOne(Integer bid) {
+//        会出现懒加载问题：org.hibernate.LazyInitializationException: could not initialize proxy - no Session
+//        return hHBookDao.getOne(bid);
+        return hHBookDao.findById(bid).get();
+    }
+
+    @RequestMapping("/getAll")
+    public List<Hbook> getAll() {
+        return hHBookDao.findAll();
+    }
+}
---
+++ springboot-data/db/springboot-jpa/src/main/java/com/bvgol/examples/springbootjpa/pojo/Hbook.java
@@ -0,0 +1,23 @@
+package com.bvgol.examples.springbootjpa.pojo;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author 黄大娘
+ * @company dogsun公司
+ * @create 2019-11-13 19:18
+ */
+@Data
+@Entity   //注释它为实体类
+@Table(name = "t_springboot_hbook") //设置要生成的表名
+public class Hbook {
+    @Id
+    @GeneratedValue
+    private Integer bid;
+    @Column(length = 100) //创建这个列值得长度
+    private String bname;
+    @Column(columnDefinition = "columnDefinition")
+    private Float price;
+}
