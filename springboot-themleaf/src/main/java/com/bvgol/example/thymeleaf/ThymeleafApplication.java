f5020131dc9d3d2fbc7cd878981253d07b03e473
guochen <bvg_Winnir@163.com>
2020-12-28 17:41:08 +0800

    springboot + thymeleaf 初探

---
+++ springboot-themleaf/src/main/java/com/bvgol/example/thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,17 @@
+package com.bvgol.example.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: my-springboot-completely
+ * @description: 启动类
+ * @author: GUOCHEN
+ * @create: 2020/12/28 17:23
+ */
+@SpringBootApplication
+public class ThymeleafApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafApplication.class, args);
+    }
+}
---
+++ springboot-themleaf/src/main/java/com/bvgol/example/thymeleaf/controller/TestController.java
@@ -0,0 +1,43 @@
+package com.bvgol.example.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @program: my-springboot-completely
+ * @description:
+ * @author: GUOCHEN
+ * @create: 2020/12/28 17:24
+ */
+@Controller
+public class TestController {
+    /**
+     * @param
+     * @return
+     * @Classname TestController
+     * @Description TODO
+     * @Date 2020/12/28 17:24
+     * @auther by GUOCHEN
+     */
+    @GetMapping("get")
+    public Object get(Model model) {
+        model.addAttribute("msg", "据说明天有大雪");
+        return "hello1";
+    }
+
+    /**
+     * @param
+     * @return
+     * @Classname TestController
+     * @Description TODO
+     * @Date 2020/12/28 17:31
+     * @auther by GUOCHEN
+     */
+    @GetMapping("getxx")
+    public Object getxx(Model model) {
+        model.addAttribute("msg", "据说明天有sssssssssss大雪");
+        return "xx/iuo";
+    }
+
+}
