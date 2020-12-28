2a68f9405dc986fbdc06a492fc69e6ec6b0bdd15
guochen <bvg_Winnir@163.com>
2020-12-28 15:07:13 +0800

    防刷

---
+++ springboot-data/nosql/springboot-redis/src/main/java/com/bvgol/examples/springbootredis/com/entity/Result.java
@@ -0,0 +1,18 @@
+package com.bvgol.examples.springbootredis.com.entity;/**
+ * @Classname Result
+ * @Description TODO
+ * @Date 2020/12/14 17:42
+ * @Created by GUOCHEN
+ */
+
+/**
+ * @program: my-springboot-completely
+ * @description:
+ * @author: GUOCHEN
+ * @create: 2020/12/14 17:42
+ */
+public class Result<T> {
+    public static Result<String> success(String s) {
+        return null;
+    }
+}
