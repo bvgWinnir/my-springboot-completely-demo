1b71b5c06bbc6084e094d8035ce14a33e47063ea
guochen <bvg_Winnir@163.com>
2021-01-20 14:11:15 +0800

    在指定位置生成二维码

---
+++ springboot-args-webflux/src/main/java/com/bvgol/examples/springbootargs/util/QRCodeGenerator.java
@@ -0,0 +1,47 @@
+package com.bvgol.examples.springbootargs.util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/**
+ * 描述：     生成二维码工具
+ */
+public class QRCodeGenerator {
+
+    /**
+     * 文件输出位置
+     */
+    private static final String QR_CODE_IMAGE_PATH = "C:\\Users\\GUOCHEN\\Desktop\\MyQRCode.png";
+
+    public static void generateQRCodeImage(String text, int width, int height, String filePath) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            String text = "二维码扫描后得到的内容";
+            Integer width = 350;
+            Integer height = 350;
+            generateQRCodeImage(text, width, height, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+        }
+
+    }
+}
