2aea544d96ec043c07d9ac6e5c3c4b8e0044f564
guochen <bvg_Winnir@163.com>
2021-01-20 14:15:18 +0800

    简单修正报错

--- springboot-mq/ActiveMQ/src/test/java/com/bvgol/examples/activemq/ActiveMqTests.java
+++ springboot-mq/ActiveMQ/src/test/java/com/bvgol/examples/activemq/ActiveMqTests.java
@@ -1,55 +1,52 @@
 package com.bvgol.examples.activemq;
 
-import com.example.demoactivemq.domain.MessageModel;
-import com.example.demoactivemq.producer.Producer;
-import org.junit.jupiter.api.Test;
+import com.bvgol.example.activemq.ActiveMqApplication;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes = DemoActivemqApplication.class)
+@SpringBootTest(classes = ActiveMqApplication.class)
 @RunWith(SpringRunner.class)
-class DemoActivemqApplicationTests {
+class ActiveMqTests {
 
-    /**
-     * 消息生产者
-     */
-    @Autowired
-    private Producer producer;
-
-    /**
-     * 及时消息队列测试
-     */
-    @Test
-    public void test() {
-        MessageModel messageModel = MessageModel.builder()
-                .message("测试消息")
-                .titile("消息000")
-                .build();
-        // 发送消息
-        producer.send(Producer.DEFAULT_QUEUE, messageModel);
-    }
-
-    /**
-     * 延时消息队列测试
-     */
-    @Test
-    public void test2() {
-        for (int i = 0; i < 5; i++) {
-            MessageModel messageModel = MessageModel.builder()
-                    .titile("延迟10秒执行")
-                    .message("测试消息" + i)
-                    .build();
-            // 发送延迟消息
-            producer.delaySend(Producer.DEFAULT_QUEUE, messageModel, 10000L);
-        }
-        try {
-            // 休眠100秒，等等消息执行
-            Thread.currentThread().sleep(100000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+//    /**
+//     * 消息生产者
+//     */
+//    @Autowired
+//    private Producer producer;
+//
+//    /**
+//     * 及时消息队列测试
+//     */
+//    @Test
+//    public void test() {
+//        MessageModel messageModel = MessageModel.builder()
+//                .message("测试消息")
+//                .titile("消息000")
+//                .build();
+//        // 发送消息
+//        producer.send(Producer.DEFAULT_QUEUE, messageModel);
+//    }
+//
+//    /**
+//     * 延时消息队列测试
+//     */
+//    @Test
+//    public void test2() {
+//        for (int i = 0; i < 5; i++) {
+//            MessageModel messageModel = MessageModel.builder()
+//                    .titile("延迟10秒执行")
+//                    .message("测试消息" + i)
+//                    .build();
+//            // 发送延迟消息
+//            producer.delaySend(Producer.DEFAULT_QUEUE, messageModel, 10000L);
+//        }
+//        try {
+//            // 休眠100秒，等等消息执行
+//            Thread.currentThread().sleep(100000L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
